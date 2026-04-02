19c47f43ff642dfda218d5c2222c4a1233b3fdc4
a1944739 <daisyrees321@gmail.com>
2026-04-03 03:15:31 +1030

    added conditional jump to compare element 1 with 1 (checks if it is 0 or below)

--- myassignment.s
+++ myassignment.s
@@ -22,6 +22,7 @@ newline: .string "\n"
 .text
 
 main:
+#loading temp variables to elements
 la t0, element1
 
 la t1, element2
@@ -32,17 +33,20 @@ la t3, element4
 
 la t4, element5
 
+#put values in
 lw t0, 0(t0)
 lw t1, 0(t1)
 lw t2, 0(t2)
 lw t3, 0(t3)
 lw t4, 0(t4)
 
+#add values up in s0
 add s0, t0, t1
 add s0, s0, t2
 add s0, s0, t3
 add s0, s0, t4
 
+#print text and final sum
 la a0, sum_text
 print_string
 
@@ -52,7 +56,22 @@ print_int
 la a0, newline
 print_string
 
+
+#checking which numbers are above 0
+
+li s2, 1
+li s1, 0
+blt t0, s2, jump2
+
+add s1,s1,s2
+mv a0, s1
+print_int
 ecall
+jump2: 
+
+ 
+
+ ecall
 done:
 
  li a7, 10
