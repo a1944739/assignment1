e666119c924f95a6e1c4ec37972b3d948d6a6b28
a1944739 <daisyrees321@gmail.com>
2026-04-03 02:38:48 +1030

    completed addition, used placeholder values

--- myassignment.s
+++ myassignment.s
@@ -2,15 +2,15 @@
 
 .data
 
-element1: .word PLACEHOLDER
+element1: .word 1
 
-element2: .word PLACEHOLDER
+element2: .word 2
 
-element3: .word PLACEHOLDER
+element3: .word 3
 
-element4: .word PLACEHOLDER
+element4: .word 4
 
-element5: .word PLACEHOLDER
+element5: .word 5
 
 num_elements: .word 5
 
@@ -19,6 +19,27 @@ num_elements: .word 5
 .text
 
 main:
+la t0, element1
+
+la t1, element2
+
+la t2, element3
+
+la t3, element4
+
+la t4, element5
+
+lw t0, 0(t0)
+lw t1, 0(t1)
+lw t2, 0(t2)
+lw t3, 0(t3)
+lw t4, 0(t4)
+
+add s0, t0, t1
+add s0, s0, t2
+add s0, s0, t3
+add s0, s0, t4
+
 
 done:
 
