cec440542e0d5458f351a16bb8aefd50b2f84754
a1944739 <daisyrees321@gmail.com>
2026-04-03 02:14:55 +1030

    Create myassignment.s

---
+++ myassignment.s
@@ -0,0 +1,27 @@
+# --- DO NOT EDIT THE VARIABLE NAMES: The autograder will use these variable names to insert testcase...
+
+.data
+
+element1: .word PLACEHOLDER
+
+element2: .word PLACEHOLDER
+
+element3: .word PLACEHOLDER
+
+element4: .word PLACEHOLDER
+
+element5: .word PLACEHOLDER
+
+num_elements: .word 5
+
+#...to here---
+
+.text
+
+main:
+
+done:
+
+ li a7, 10
+
+ ecall
